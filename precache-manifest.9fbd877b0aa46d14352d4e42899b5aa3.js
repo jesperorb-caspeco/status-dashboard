d314d8d1ff11eb9c0991bb81fb72e9996d059870
Jesper Orb <jesper.orb@caspeco.se>
2019-03-13 12:42:30 +0100

    Updates

---
+++ precache-manifest.9fbd877b0aa46d14352d4e42899b5aa3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c197ca000064f8e0b8d5",
+    "url": "/status-dashboard/static/js/runtime~main.a539c1d2.js"
+  },
+  {
+    "revision": "3959b63f13b22fb8949d",
+    "url": "/status-dashboard/static/js/main.4f42b320.chunk.js"
+  },
+  {
+    "revision": "8e9e11c14cb7a1f03046",
+    "url": "/status-dashboard/static/js/2.614cb13f.chunk.js"
+  },
+  {
+    "revision": "3959b63f13b22fb8949d",
+    "url": "/status-dashboard/static/css/main.cdb63d98.chunk.css"
+  },
+  {
+    "revision": "8ce753e76a317b1ec89644c52bf1e023",
+    "url": "/status-dashboard/index.html"
+  }
+];
